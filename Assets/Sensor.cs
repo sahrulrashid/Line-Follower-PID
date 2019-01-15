45d8d1421258fa907a72cdf2d539686588a7c107
vvrvvd <vvrvvd@gmail.com>
2019-01-15 21:14:01 +0100

    Init

---
+++ Assets/Sensor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensor : MonoBehaviour {
+
+    [SerializeField]
+    private LayerMask lineMask;
+
+    [SerializeField]
+    private Material onMaterial;
+
+    [SerializeField]
+    private Material offMaterial;
+
+    private int state;
+    private Renderer rend;
+
+    void Awake()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void FixedUpdate () {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 10, lineMask))
+        {
+            state = 1;
+            rend.material = onMaterial;
+        }
+        else
+        {
+            state = 0;
+            rend.material = offMaterial;
+        }
+    }
+
+    public int GetState()
+    {
+        return state;
+    }
+}
---
+++ Assets/WheelEngine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelEngine : MonoBehaviour {
+
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public float maxPower;
+
+    void Update () {
+		if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rightWheel.brakeTorque = 0;
+            rightWheel.motorTorque = maxPower;
+        }
+        else
+        {
+            rightWheel.brakeTorque = Mathf.Infinity;
+            rightWheel.motorTorque = 0;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            leftWheel.motorTorque = maxPower;
+            leftWheel.brakeTorque = 0;
+        }
+        else
+        {
+            leftWheel.brakeTorque = Mathf.Infinity;
+            leftWheel.motorTorque = 0;
+        }
+    }
+}
